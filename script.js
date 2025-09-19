c3b84a260bb9b77a89bc2723669b09f56c424bc3
Sivaraju Chakravaram <sivarajuchakravaram287@gmail.com>
2025-09-19 20:40:43 +0530

    script.js

--- script.js
+++ script.js
@@ -56,6 +56,78 @@ function getWeather() {
       <p>💨 Wind: ${data.wind} km/h</p>
     `;
   } else {
-    result.innerHTML = `<p style="color:red;">State/UT not found. Example: andhra pradesh, delhi, tamil nadu</p>`;
+    result.innerHTML = `<p style="color:red;">State/UT not found. Example: Andhra Pradesh
+
+Arunachal Pradesh
+
+Assam
+
+Bihar
+
+Chhattisgarh
+
+Goa
+
+Gujarat
+
+Haryana
+
+Himachal Pradesh
+
+Jharkhand
+
+Karnataka
+
+Kerala
+
+Madhya Pradesh
+
+Maharashtra
+
+Manipur
+
+Meghalaya
+
+Mizoram
+
+Nagaland
+
+Odisha
+
+Punjab
+
+Rajasthan
+
+Sikkim
+
+Tamil Nadu
+
+Telangana
+
+Tripura
+
+Uttar Pradesh
+
+Uttarakhand
+
+West Bengal
+
+
+
+Andaman and Nicobar Islands
+
+Chandigarh
+
+Dadra and Nagar Haveli and Daman and Diu
+
+Delhi (National Capital Territory)
+
+Jammu and Kashmir
+
+Ladakh
+
+Lakshadweep
+
+Puducherry</p>`;
   }
 }
